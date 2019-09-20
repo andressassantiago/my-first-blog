e7e32c5ac47732d9928d17c74571af0a37d977c7
andressassantiago <50993008+andressassantiago@users.noreply.github.com>
2019-09-20 09:52:58 -0300

    Add files via upload

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+<<<<<<< HEAD
 ]
+=======
+]
+>>>>>>> 2e57dd939b904e16a1092ac1c317b2d6472e2976
